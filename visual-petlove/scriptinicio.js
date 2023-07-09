f50e499d810472fdc8957f71ffe92ec80e950af0
lauraromeroforero <en17la@gmail.com>
2023-07-09 14:49:11 -0500

    commit andres

--- visual-petlove/scriptinicio.js
+++ visual-petlove/scriptinicio.js
@@ -3,7 +3,14 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Aquí puedes agregar la lógica de autenticación
-    // por ejemplo, enviar los datos al servidor para verificar las credenciales
-    // y redirigir al usuario a la página de inicio después de iniciar sesión exitosamente
+    if (email.endsWith('@gmail.com')) {
+      window.location.href = 'Cliente/clienteprincipal.html'; // Redireccionar a la página del cliente
+    } else if (email.endsWith('@professionalgrooming.com')) {
+      window.location.href = 'Veterinario/veterinarioprincipal.html'; // Redireccionar a la página del veterinario
+    } else if (email.endsWith('@petlove.com')) {
+      window.location.href = 'Admin/adminprincipal.html'; // Redireccionar a la página del administrador
+    } else {
+        // Si no coincide con ninguno, se puede mostrar un mensaje de error o tomar otra acción
+        alert('Correo electrónico no válido');
+    }
 });
