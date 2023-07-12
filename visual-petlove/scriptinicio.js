051efe4a70631ad1bfee30901976294b1fe4ac26
lauraromeroforero <en17la@gmail.com>
2023-07-11 19:36:45 -0500

    inicio sesion

--- visual-petlove/scriptinicio.js
+++ visual-petlove/scriptinicio.js
@@ -1,34 +1,33 @@
-$(document).ready(function() {
-  $('.login').on('click', function() {
+$(document).ready(function () {
+  $('.login').on('click', function (event) {
+    event.preventDefault();
     let correo = $('#email').val();
-    let contraseña = $('#password').val();
-
-    if (!$('#respuesta').html()) {
-      $.ajax({
-        url: "http://localhost:8080/BuscarUsuarioEmail/" + correo,
-        type: "GET",
-        datatype: "JSON",
-        success: function(respuesta) {
-          console.log(respuesta);
-          document.getElementById('loginForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
-        
-            if (email.endsWith('@gmail.com')) {
+    let contrasena = $('#password').val();
+    $.ajax({
+      url: "http://localhost:8080/BuscarUsuarioEmail/" + correo,
+      type: "GET",
+      datatype: "JSON",
+      success: function (respuesta) {
+        console.log(respuesta);
+        if (respuesta.length != 0) {
+          if (respuesta[0].contrasena_usuario == contrasena) {
+            if (correo.endsWith('@gmail.com')) {
               window.location.href = 'Cliente/clienteprincipal.html'; // Redireccionar a la página del cliente
-            } else if (email.endsWith('@professionalgrooming.com')) {
+            } else if (correo.endsWith('@professionalgrooming.com')) {
               window.location.href = 'Veterinario/veterinarioprincipal.html'; // Redireccionar a la página del veterinario
-            } else if (email.endsWith('@petlove.com')) {
+            } else if (correo.endsWith('@petlove.com')) {
               alert('Correo electrónico encontrado ¡Navegue feliz!');
               window.location.href = 'Admin/adminprincipal.html'; // Redireccionar a la página del administrador
-            } else {
-                // Si no coincide con ninguno, se puede mostrar un mensaje de error o tomar otra acción
-                alert('Correo electrónico no encontrado');
             }
-        });
+          } else {
+            // Si no coincide con ninguno, se puede mostrar un mensaje de error o tomar otra acción
+            alert('Contraseña o usuario incorrecto');
+          }
+        } else {
+          // Si no coincide con ninguno, se puede mostrar un mensaje de error o tomar otra acción
+          alert('Correo electrónico no encontrado');
         }
-      });
-    }
+      }
+    });
   });
 });
--- visual-petlove/scriptregistro.js
+++ visual-petlove/scriptregistro.js
@@ -1,8 +1,50 @@
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const phone = document.getElementById('phone').value;
-    const birthdate = document.getElementById('birthdate').value;
+function selectRol(){
+    const rolselect = document.querySelector("#rol");
+    rolselect.innerHTML = "<option value='Seleccione Rol'>Rol A elegir</option>";
+
+    $.ajax({
+        url: "http://localhost:8080/ListarRolUsuario",
+        type: "GET",
+        datatype: "JSON",
+        success: function (respuesta){
+            console.log(respuesta)
+            Object.values(respuesta).forEach(rol => {
+                rolselect.innerHTML += '<option value="' + rol['id_rol_usuario'] +'">' + rol['nombre_rol_usuario'] + '</option>'
+            });
+        }
+    })
+}
+
+$(document).ready(function() {
+    selectRol();
+    const GuardaUsuario = $("#GuardaUsuario");
+    const nombre = $('#nombre');
+    const apellido = $('#apellido');
+    const direccion = $('#direccion');
+    const telefono = $('#telefono');
+    const correo = $('#correo');
+    const contrasena = $('#contrasena');
+    const rolselect = $("#rolselect");
+
+    GuardaUsuario.click(function(event) {
+        event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+
+        const nombreseleccionado = nombre.val();
+        const apellidoseleccionado = apellido.val();
+        const direccionseleccionado = direccion.val();
+        const telefonoseleccionado = telefono.val();
+        const correoseleccionado = correo.val();
+        const contrasenaseleccionado = contrasena.val();
+        const rolseleccionado = rolselect.val();
+
+        $.ajax({
+            url: "http://localhost:8080/AgregarUsuario/" + rolseleccionado,
+            type: "POST",
+            dataType: "JSON",
+            success: function(response) {
+                console.log(response)
+                console.log("Agregado")
+            },
+        });
+    });
 });
